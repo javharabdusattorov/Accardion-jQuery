ca07a4db75670914ce3d23292294ed888ad37a34
javharabdusattorov <javharabdusattorov2008@gmail.com>
2022-04-24 23:25:35 +0500

    correct of the function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-class accardion {
-    constructor(item) {
-        $(item).find('.accardion-body').not(':nth(0)').hide();
-        $(item).find('.accardion-header').click(function() {
-            $(item).find(this).next().slideToggle(400);
-            $(item).find('.accardion-body').not($(this).next()).slideUp(400);
-        })  
-    }
-} 
-const accardionParent = $('.accardion');
-for(let i of accardionParent) {
-    i = new accardion(i);
+function createAccordion(accordionEl) {
+    accordionEl.find(".accardion-body").not(":nth(0)").hide();
+    accordionEl.find(".accardion-header").click(function () {
+      const thisBody = accordionEl.find(this).next();
+      const otherBodies = accordionEl.find(".accardion-body").not(thisBody);
+
+      thisBody.slideToggle(400);
+      otherBodies.slideUp(400);
+    });
+}
+
+const accardionParents = $(".accardion");
+for (let i of accardionParents) {
+    createAccordion($(i))
 }
